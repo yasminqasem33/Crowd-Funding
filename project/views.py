5e98364961e2bc1a577b3d2d6817070f5909af8a
yasminqasem <yasminqasem11@gmail.com>
2019-04-25 19:21:03 +0200

    function show project

--- project/views.py
+++ project/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def showOne(request,id):
+    project = Project.objects.get(id=id)
+    return render(request, 'project/showOne.html', {"project": project})
+
+
