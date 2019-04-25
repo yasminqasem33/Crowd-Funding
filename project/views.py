8941c9e99a85ce88a2fd564e312f7f00ab1199e4
yasminqasem <yasminqasem11@gmail.com>
2019-04-25 22:44:04 +0200

    link view with template in view project

--- project/views.py
+++ project/views.py
@@ -4,6 +4,7 @@ from .models import Project
 # Create your views here.
 def showOne(request,id):
     project = Project.objects.get(id=id)
+    print(project)
     return render(request, 'project/showOne.html', {"project": project})
 
 
