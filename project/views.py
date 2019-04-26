a3affb7fed6812721f2fbd3d2f56c12ce5b2323e
ArwaMohsen <arwamohsen22@gmail.com>
2019-04-26 02:50:39 +0200

    listing each category's project

--- project/views.py
+++ project/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
+def category(request,id):
+    category = Category.object.get(id=id)
+    projects = Project.objects.get(category=category)
+    return render(request, 'project/category.html', {"category": category, "projects":projects})
+
 def list_cates(request):
     categories = Category.objects.all()
     categories_names = list()
     for category in categories:
         categories_names.append(category.name)
         print(category.name)
-    return render(request, 'project/list_cates', {"categories": categories})
+    return render(request, 'project/list_cates.html', {"categories": categories})
 
 def showOne(request,id):
     project = Project.objects.get(id=id)
