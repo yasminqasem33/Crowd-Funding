5f639cb7015d275703141390b99612489f483928
yasminqasem <yasminqasem11@gmail.com>
2019-05-02 16:56:45 +0200

    latest changes

--- crowdFunding/settings.py
+++ crowdFunding/settings.py
@@ -77,10 +77,8 @@ WSGI_APPLICATION = 'crowdFunding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'crowdFunding',
-        'USER': 'root',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
